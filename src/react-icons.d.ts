fe37451313b145a2ee8a9d1ad97751554f1fd110
Tich Zvidzayi <tzvidzie@gmail.com>
2026-01-15 14:00:40 +0200

    fix-deployment

---
+++ src/react-icons.d.ts
@@ -0,0 +1,11 @@
+declare module 'react-icons/fa' {
+  import { FC, SVGProps } from 'react';
+  export const FaUser: FC<SVGProps<SVGSVGElement>>;
+  export const FaGraduationCap: FC<SVGProps<SVGSVGElement>>;
+  export const FaBriefcase: FC<SVGProps<SVGSVGElement>>;
+  export const FaMapMarkerAlt: FC<SVGProps<SVGSVGElement>>;
+  export const FaEnvelope: FC<SVGProps<SVGSVGElement>>;
+  export const FaGithub: FC<SVGProps<SVGSVGElement>>;
+  export const FaLinkedin: FC<SVGProps<SVGSVGElement>>;
+  export const FaExclamationTriangle: FC<SVGProps<SVGSVGElement>>;
+}
